1e2a6635b726b2c4e6d5ba2ef6bf7ae0b361dbb2
emard <vordah@gmail.com>
2020-07-23 21:36:18 +0200

    bidirectional MDV BRAM

--- src/mdv_bram.v
+++ src/mdv_bram.v
@@ -3,13 +3,14 @@ module mdv_bram (
   input            we_a,
   input      [9:0] addr_a, addr_b,
   input      [7:0] din_a,
-  output reg [7:0] dout_b
+  output reg [7:0] dout_a, dout_b
 );
   reg [7:0] ram [0:1023];
   always @(posedge clk) begin
     if (we_a) begin
       ram[addr_a] <= din_a;
     end
+    dout_a <= ram[addr_a];
     dout_b <= ram[addr_b];
   end
 endmodule
